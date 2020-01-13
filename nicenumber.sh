b5f6cf24c85811fca091ee42969627b36204e864
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2020-01-13 13:46:11 +0530

    Create nicenumber.sh

---
+++ nicenumber.sh
@@ -0,0 +1,5 @@
+cat << "EOF" >&2
+Usage: $(basename $0) [-d c] [-t c] numeric value
+      -d specifies the decimal point delimiter (default '.')
+      -t specifies the thousands delimiter (default ',')
+EOF
